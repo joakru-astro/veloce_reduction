a9b0f18e5ba1cc4d73bdf87eeca87bb0825c570d
joakru-astro <jj.kruger@icloud.com>
2025-02-06 18:27:43 +1000

    switch to running the reduction with configuration file

---
+++ simple_veloce_reduction/veloce_config.py
@@ -0,0 +1,157 @@
+### Put here the directory structure for the Veloce reduction pipeline
+import os
+import yaml
+
+data_dirs = {'red': 'ccd_3', 'green': 'ccd_2', 'blue': 'ccd_1'}
+
+class VelocePaths:
+    # def __init__(self, input_path, output_path, run=None):
+    def __init__(self, run=None):
+        self.reduction_parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))#'~/Joachim_veloce/Veloce_reduction/'
+        # print(self.reduction_parent_dir)
+        self.raw_parent_dir = os.path.join(self.reduction_parent_dir, 'Data', 'Raw')
+        self.extracted_parent_dir = os.path.join(self.reduction_parent_dir, 'Data', 'Extracted')
+        self.wave_dir = os.path.join(self.reduction_parent_dir, 'Wave')
+        self.trace_dir = os.path.join(self.reduction_parent_dir, 'Trace')
+        self.blaze_dir = os.path.join(self.reduction_parent_dir, 'Blaze')
+        self.master_dir = os.path.join(self.reduction_parent_dir, 'Master')
+        self.obs_list_dir = os.path.join(self.reduction_parent_dir, 'Obs_lists')
+        self.run = run
+        if self.run is not None:
+            self.__post_init__()
+
+    def __post_init__(self):
+        # if self.run is not None:
+        self.raw_dir = os.path.join(self.raw_parent_dir, f'{self.run}/')
+        self.extracted_dir = os.path.join(self.extracted_parent_dir, f'{self.run}/')
+
+    def update_run(self, run):
+        self.run = run
+        self.__post_init__()
+
+    @classmethod
+    def from_config(cls, config):
+        paths = cls()
+        # paths.reduction_parent_dir = config['reduction_parent_dir']
+        if not os.path.isabs(config['raw_parent_dir']):
+            paths.raw_parent_dir = os.path.join(os.getcwd(), config['raw_parent_dir'])
+        else:
+            paths.raw_parent_dir = config['raw_parent_dir']
+        if not os.path.isabs(config['extracted_parent_dir']):
+            paths.extracted_parent_dir = os.path.join(os.getcwd(), config['extracted_parent_dir'])
+        else:
+            paths.extracted_parent_dir = config['extracted_parent_dir']
+        # internal paths
+        if config['wave_dir'] != 'Default':
+            paths.wave_dir = config['wave_dir']
+        if config['trace_dir'] != 'Default':
+            paths.trace_dir = config['trace_dir']
+        # intermediate paths
+        if config['blaze_dir'] != 'Default':
+            paths.blaze_dir = config['blaze_dir']
+        if config['master_dir'] != 'Default':
+            paths.master_dir = config['master_dir']
+        if config['obs_list_dir'] != 'Default':
+            paths.obs_list_dir = config['obs_list_dir']
+        # if config['plot_dir'] != 'Default':
+        #     paths.plot_dir = config['plot_dir']
+        # if config['log_dir'] != 'Default':
+        #     paths.log_dir = config['log_dir']
+        paths.update_run(config['run'])
+        return paths
+    
+    def __repr__(self):
+        return f'VelocePaths({self.run})'
+    
+    def __str__(self):
+        message = f'VelocePaths instance for run: {self.run}\n'
+        message += f'Raw data directory: {self.raw_dir}\n'
+        message += f'Extracted data directory: {self.extracted_dir}\n'
+        message += f'Wave directory: {self.wave_dir}\n'
+        message += f'Trace directory: {self.trace_dir}\n'
+        return message
+    # remove methods below if not needed
+    def __eq__(self, other):
+        equal = True
+        for key, value in self.__dict__.items():
+            if value != other.__dict__[key]:
+                equal = False
+                break
+        return equal
+    
+    def __ne__(self, other):
+        return not self.__eq__(other)
+    
+    def __hash__(self):
+        return hash(tuple(self.__dict__.values()))
+    
+    def __getitem__(self, key):
+        return self.__dict__[key]
+    
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+
+    
+def load_config(config_file):
+    if not os.path.isabs(config_file):
+        config_file = os.path.join(os.getcwd(), config_file)
+
+    with open(config_file, 'r') as f:
+        config = yaml.safe_load(f)
+    if validate_config(config):
+        veloce_paths = VelocePaths.from_config(config)
+        science_targets = load_target_list(config['science_targets'])
+        config['science_targets'] = science_targets
+        config = {key: value for key, value in config.items() if 'dir' not in key}
+        return config, veloce_paths
+    else:
+        raise ValueError('Invalid configuration file')
+
+def validate_config(config):
+    # validate options set in config file
+    if config['reduce'] not in ['run', 'night', 'file']:
+        raise ValueError('reduce must be "run", "night" or "file", not {config["reduce"]}')
+    if config['calib_type'] not in ['SimLC', 'SimThXe', 'Interpolate', 'Static']:
+        raise ValueError('calib_type must be "SimLC", "SimThXe", "Interpolate" or "Static", not {config["calib_type"]}')
+    if config['arm'] not in ['all', 'red', 'green', 'blue']:
+        raise ValueError('arm must be "all", "red", "green" or "blue", not {config["arm"]}')
+    if config['amplifier_mode'] not in [2, 4]:
+        raise ValueError('amplifier_mode must be 2 or 4, not {config["amplifier_mode"]}')
+    if not isinstance(config['plot_diagnostic'], bool):
+        raise ValueError('plot_diagnostic must be True or False, not {config["plot_diagnostic"]}')
+    # validate targets list
+    if not (os.path.exists(os.path.join(os.getcwd(), config['science_targets'])) or os.path.exists(config['science_targets'])):
+        raise FileNotFoundError(f'{config["science_targets"]} does not exist')
+    else:
+        pass # maybe verify if targets are present for the run/night/file
+    # validate input paths
+    if not os.path.exists(config['raw_parent_dir']):
+        raise FileNotFoundError(f'{config["raw_parent_dir"]} does not exist')
+    elif not os.path.exists(os.path.join(config['raw_parent_dir'], config['run'])):
+        raise FileNotFoundError(f'{config["run"]} does not exist in {config["raw_parent_dir"]}')
+    if config['reduce'] == 'night' and not os.path.exists(os.path.join(config['raw_parent_dir'], config['run'], config['date'])):
+        raise FileNotFoundError(f'{config["date"]} does not exist in {config["raw_parent_dir"]}/{config["run"]}')
+    # if config['reduce'] == 'file' and not os.path.exists(os.path.join(config['raw_parent_dir'], config['run'], config['date'], config['filename'])):
+    #     raise FileNotFoundError(f'{config["filename"]} does not exist in {config["raw_parent_dir"]}/{config["run"]}/{config["date"]}')
+    # validate output paths
+    if not os.path.exists(config['extracted_parent_dir']):
+        raise FileNotFoundError(f'{config["extracted_parent_dir"]} does not exist')
+    # validate internal paths
+    if config['wave_dir'] != 'Default' and not os.path.exists(config['wave_dir']):
+        raise FileNotFoundError(f'{config["wave_dir"]} does not exist')
+    if config['trace_dir'] != 'Default' and not os.path.exists(config['trace_dir']):
+        raise FileNotFoundError(f'{config["trace_dir"]} does not exist')
+    
+    return True
+# blaze_dir: Default
+# master_dir: Default
+# obs_list_dir: Default
+# plot_dir: Default
+# log_dir: Default
+
+def load_target_list(target_file):
+    if not os.path.isabs(target_file):
+        target_file = os.path.join(os.getcwd(), target_file)
+    with open(target_file, 'r') as f:
+        targets = f.read().splitlines()
+    return targets
---
+++ veloce_reduction.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import yaml
+import os
+from astropy.io import fits
+from simple_veloce_reduction import veloce_reduction_tools, veloce_extraction, veloce_logs, veloce_config
+
+def main():
+    # Load configuration
+    # config_filename = '/home/usqobserver2/Joachim_veloce/veloce_reduction_dev/veloce_reduction/config.yaml'
+    # config, veloce_paths = veloce_config.load_config(config_filename)
+
+    config, veloce_paths = veloce_config.load_config('config.yaml')
+
+    if config['reduce'] == 'run':
+        # Run the reduction
+        obs_list = veloce_logs.load_run_logs(
+            config['run'], config['science_targets'], config['arm'])
+        obs_list = veloce_logs.get_obs_list(
+            obs_list['science'])
+        veloce_extraction.extract_run_no_blaze(
+            obs_list, config['run'],
+            config['arm'], config['amplifier_mode'],
+            sim_calib=config['sim_calib'], remove_background=config['scattered_light'],
+            veloce_paths=veloce_paths)
+    elif config['reduce'] == 'night':
+        obs_list = veloce_logs.load_night_logs(
+            config['run'], config['date'], config['science_targets'], config['arm'])
+        obs_list = veloce_logs.get_obs_list(
+            obs_list['science'])
+        veloce_extraction.extract_night_no_blaze(
+            obs_list, config['run'], config['date'],
+            config['arm'], config['amplifier_mode'],
+            sim_calib=config['sim_calib'], remove_background=config['scattered_light'],
+            veloce_paths=veloce_paths)
+    elif config['reduce'] == 'file':
+        # obs_list = veloce_logs.load_night_logs(
+        #     config['run'], config['date'], config['science_targets'], config['arm'])
+        veloce_extraction.extract_single_file(
+            config['filename'], config['run'], config['date'],
+            config['arm'], config['amplifier_mode'],
+            sim_calib=config['sim_calib'], remove_background=config['scattered_light'],
+            veloce_paths=veloce_paths)
+
+if __name__ == "__main__":
+    main()
